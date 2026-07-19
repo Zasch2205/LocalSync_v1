ad29ea97e63036846e2a801d346f8faed6b117a1
Zasch <s.molina@gmx.de>
2026-07-19 23:07:54 +0200

    Add Xcode project files to main repository

---
+++ app/LocalSync_v1/LocalSync_v1/LocalSync_v1App.swift
@@ -0,0 +1,17 @@
+//
+//  LocalSync_v1App.swift
+//  LocalSync_v1
+//
+//  Created by Sascha Molina on 19.07.26.
+//
+
+import SwiftUI
+
+@main
+struct LocalSync_v1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ app/LocalSync_v1/LocalSync_v1Tests/LocalSync_v1Tests.swift
@@ -0,0 +1,19 @@
+//
+//  LocalSync_v1Tests.swift
+//  LocalSync_v1Tests
+//
+//  Created by Sascha Molina on 19.07.26.
+//
+
+import Testing
+@testable import LocalSync_v1
+
+struct LocalSync_v1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
